572cc81c8d8d7399793f4ba4f60630250c63a7ad
ljr4488 <ljr4488@naver.com>
2021-09-09 00:00:39 +0900

    Signed-off-by: ljr4488 <ljr4488@naver.com>

---
+++ src/test4.java
@@ -0,0 +1,13 @@
+
+import junit.framework.TestCase;
+import com.scm.diff.*;
+
+public class test extends TestCase {
+	public void testMain() throws Exception {
+		 String[] args = new String[3];
+		 args[0] = "3017";
+//		 
+		 diff.main(args);
+	}
+
+}
